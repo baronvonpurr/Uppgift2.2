a813b21d83f2254474a942d7de720189a5b655d8
baronvonpurr <robinhood.svensson@hotmail.com>
2023-09-08 13:39:48 +0200

    Add project files.

---
+++ Uppgift2.2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Uppgift2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vilket år är det?");
+            int nuår = int.Parse(Console.ReadLine());
+            int årsen = nuår + 20;
+            Console.WriteLine("Om 20 år så är det " + årsen);
+        }
+    }
+}
